38823f6ef5be22cb75995b1548a0e76e3179ffb1
Eric Liu <ericyl.us@gmail.com>
2019-07-01 20:45:21 -0700

    feat(src): add cache and info

--- src/FuzzyHighlight.tsx
+++ src/FuzzyHighlight.tsx
@@ -1,11 +1,90 @@
+import Fuse from 'fuse.js';
 import * as React from 'react';
 
-interface IFuzzyHighlightProps {}
+class FuzzyHighlight<T, O> extends React.Component<
+  IFuzzyHighlightProps<T, O>,
+  IFuzzyHighlightState<T>
+> {
+  public readonly state: IFuzzyHighlightState<T> = {
+    results: [],
+    cache: {},
+    info: {
+      timing: 0
+    }
+  };
+  private fuse!: Fuse<T, Options<O>>;
 
-class FuzzyHighlight extends React.Component<IFuzzyHighlightProps> {
+  public componentDidMount() {
+    const { data, options } = this.props;
+    this.fuse = new Fuse(data, options || {});
+    this.search();
+  }
+
+  public componentDidUpdate(prevProps: IFuzzyHighlightProps<T, O>) {
+    if (prevProps.query !== this.props.query) {
+      this.search();
+    }
+  }
+
   public render() {
+    const { children } = this.props;
+    const { results, info } = this.state;
+
+    if (children) {
+      return children({ results, timing: info.timing });
+    }
+
     return null;
   }
+
+  public search() {
+    const { query } = this.props;
+    const { cache } = this.state;
+
+    if (Object(cache).hasOwnProperty(query)) {
+      return this.setState({
+        results: cache[query],
+        info: {
+          timing: 0
+        }
+      });
+    }
+
+    const start = window.performance.now();
+    const results = this.fuse.search(query);
+    const end = window.performance.now();
+
+    this.setState({
+      results,
+      cache: {
+        ...cache,
+        [query]: results
+      },
+      info: {
+        timing: parseFloat((end - start).toFixed(3))
+      }
+    });
+  }
+}
+
+type Data<T> = ReadonlyArray<T>;
+type Options<O> = Fuse.FuseOptions<Readonly<O>>;
+
+interface IFuzzyHighlightProps<T, O> {
+  query: string;
+  data: Data<T>;
+  options?: Options<O>;
+  children?: (params: { results: Data<T>; timing: number }) => React.ReactNode;
+}
+
+interface IFuzzyHighlightState<T> {
+  results: Data<T>;
+  cache: {
+    [query: string]: Data<T>;
+  };
+  info: {
+    timing: number;
+  };
 }
 
 export default FuzzyHighlight;
