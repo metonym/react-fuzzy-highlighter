e652f9b23e15b0847105d404e05235103f795b4e
Eric Liu <ericyl.us@gmail.com>
2019-07-01 20:47:40 -0700

    feat(examples): use component with fixture data

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,10 +1,58 @@
 import * as React from 'react';
 import FuzzyHighlight from 'react-fuzzy-highlight';
+import data from './data/default.json';
 
-class App extends React.Component {
+interface IAppState {
+  query: string;
+  data: any[];
+}
+
+class App extends React.Component<{}, IAppState> {
+  public state: IAppState = {
+    query: '',
+    data
+  };
+
   public render() {
-    return <FuzzyHighlight />;
+    return (
+      <>
+        <input
+          type="search"
+          value={this.state.query}
+          onChange={this.handleChange}
+        />
+        <FuzzyHighlight
+          query={this.state.query}
+          data={this.state.data}
+          options={{
+            shouldSort: true,
+            includeMatches: true,
+            threshold: 0.6,
+            location: 0,
+            distance: 100,
+            maxPatternLength: 32,
+            minMatchCharLength: 1,
+            keys: ['title', 'author.firstName']
+          }}
+        >
+          {({ results, timing }) => {
+            return (
+              <>
+                <div>time: {timing}ms</div>
+                {JSON.stringify(results, null, 2)}
+              </>
+            );
+          }}
+        </FuzzyHighlight>
+      </>
+    );
   }
+
+  private handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      query: e.target.value
+    });
+  };
 }
 
 export default App;
