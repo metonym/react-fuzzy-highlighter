e75d09d0503f03cfa58e21f2da099650fd0ccc21
Eric Liu <ericyl.us@gmail.com>
2019-07-01 20:44:07 -0700

    chore(scripts): disable source maps

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-find ./docs -type f ! -name '*.md' -delete
-cd examples/
-yarn build -d ../docs
+find ./docs/ -type f ! -name '*.md' -delete
+cd ./examples/
+yarn build --no-source-maps -d ../docs/
 
 exit 0;
