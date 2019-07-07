69d03b8612a91f265a4f51d49024cf2814e6ae9d
Eric Liu <ericyl.us@gmail.com>
2019-07-07 12:23:01 -0700

    test: add highlighter tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>src/**/*.(spec|test).{ts,tsx}'],
-  setupFilesAfterEnv: ['<rootDir>src/tests/setup.ts']
+  setupFilesAfterEnv: ['<rootDir>src/tests/setup.ts'],
+  snapshotSerializers: ['enzyme-to-json/serializer']
 };
---
+++ src/tests/Highlighter.spec.tsx
@@ -0,0 +1,30 @@
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import * as React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { Highlighter } from '../Highlighter';
+
+describe('Highlighter', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    render(<Highlighter text="" />, div);
+    unmountComponentAtNode(div);
+  });
+
+  test('prop `text` as an array', () => {
+    const wrapper = shallow(
+      <Highlighter
+        text={[
+          { text: 'O', isHighlighted: true },
+          { text: 'ld', isHighlighted: false }
+        ]}
+      />
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  test('prop `text` as a string', () => {
+    const wrapper = shallow(<Highlighter text={'Old'} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
