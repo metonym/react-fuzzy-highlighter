c6849698f97ee2104e70c18155bd314331059076
Eric Liu <ericyl.us@gmail.com>
2019-07-13 11:47:00 -0700

    chore(format results): fix typings

--- src/formatResults.ts
+++ src/formatResults.ts
@@ -2,36 +2,48 @@ import set from 'set-value';
 import strind from 'strind';
 import { Result, Results } from './FuzzyHighlighter';
 
-interface IFinalResult<T> extends Result<T> {
-  formatted: {
-    [key: string]: $FixMe;
-  };
-}
-
-export type FinalResults<T> = Array<IFinalResult<T>>;
-
 function formatResults<T>(results: Results<T>): FinalResults<T> {
   const finalResults: FinalResults<T> = [];
+  const mapTextMatches = ({
+    chars: text,
+    matches
+  }: {
+    chars: string;
+    matches: boolean;
+  }) => ({ text, isHighlighted: matches });
 
   results.forEach((result, index) => {
     finalResults.push({ ...result, formatted: { ...result.item } });
     result.matches.forEach(({ indices, key, value }: IFuzzyResult) => {
-      const { matched } = strind(
-        value,
-        indices,
-        ({ chars: text, matches }) => ({ text, isHighlighted: matches })
-      );
+      const output = strind(value, indices, mapTextMatches);
+      const formattedResult = output.matched as IFormattedResult[];
+      const formatted = finalResults[index].formatted as IFormatted;
 
       if (key.split('.').length > 1) {
-        set(finalResults[index].formatted, key, matched);
+        set(formatted, key, formattedResult);
       } else {
-        finalResults[index].formatted[key] = matched;
+        formatted[key] = formattedResult;
       }
     });
   });
 
   return finalResults;
 }
+
+interface IFormattedResult {
+  text: string;
+  isHighlighted: boolean;
+}
+
+interface IFormatted {
+  [key: string]: IFormattedResult[];
+}
+
+interface IFinalResult<T> extends Result<T> {
+  formatted: T | IFormatted;
+}
+
+export type FinalResults<T> = Array<IFinalResult<T>>;
 
 interface IFuzzyResult {
   arrayIndex: number;
