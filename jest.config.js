91a4a9c9ef39ec126c85e4098e321b6962a4ff7d
Eric Liu <ericyl.us@gmail.com>
2019-07-14 11:54:23 -0700

    chore: add jest config to package.json

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>src/**/*.(spec|test).{ts,tsx}'],
-  setupFilesAfterEnv: ['<rootDir>src/tests/setup.ts'],
-  snapshotSerializers: ['enzyme-to-json/serializer']
-};
