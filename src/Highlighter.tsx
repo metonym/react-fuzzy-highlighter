a74c2cabcde38c3826be26b23ceaddde5c8179c5
Linda Paiste <lindapaiste@gmail.com>
2022-02-07 13:52:59 -0600

    Allow the <mark> around highlighted text to be replaced with a custom element.

--- src/Highlighter.tsx
+++ src/Highlighter.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 
 function Highlighter(props: IHighlighterProps) {
   if (Array.isArray(props.text)) {
+    const Mark = props.mark || 'mark';
     return (
       <>
         {props.text.map(({ text, isHighlighted }, index) => {
           const key = `${text}${index}`; // TODO: generate id
 
           if (isHighlighted) {
-            // TODO: allow custom highlight element
-            return <mark key={key}>{text}</mark>;
+            return <Mark key={key}>{text}</Mark>;
           }
 
           return <React.Fragment key={key}>{text}</React.Fragment>;
@@ -22,7 +22,16 @@ function Highlighter(props: IHighlighterProps) {
 }
 
 interface IHighlighterProps {
+  /**
+   * The text to display.
+   * Either a `string` or an array of formatted results from the FuzzyHighlighter.
+   */
   text: string | { text: string; isHighlighted: boolean }[];
+  /**
+   * Custom JSX element to surround highlighted text.
+   * Default: 'mark'
+   */
+  mark?: keyof JSX.IntrinsicElements | React.ComponentType;
 }
 
 export { Highlighter };
